d3de5baaf1311ba65225a05e8ea4f22d9d2ae605
sasankgit <sasank4pr@gmail.com>
2025-09-28 21:50:09 +0530

    working and semi styled

--- src/pages/chatpage.jsx
+++ src/pages/chatpage.jsx
@@ -4,8 +4,8 @@ import axios from "axios";
 
 
 
-export async function generateanswer(question){
-    console.log("called");
+export async function generateanswer(question,setAnswer){
+    setAnswer("called");
     
     
     
@@ -24,25 +24,28 @@ export async function generateanswer(question){
             ]
         }
     })
-    console.log(response['data']['candidates'][0]['content']['parts'][0]['text']);
+    setAnswer(response['data']['candidates'][0]['content']['parts'][0]['text'])
+
 }
 
 
 
 export function ChatPage(){
     const [question, setQuestion] = React.useState("");
+    const [answer,setAnswer] = React.useState("");
 
     const handleclick = () => {
-        generateanswer(question);
+        generateanswer(question , setAnswer);
     }
     
     
     return(
         <div className="w-full h-full flex flex-col items-center justify-center">
-            <h1>Chat Page</h1>
+            <h1 className="bg-red-500 ">Chat Page</h1>
             <p>welcome to the chat page</p>
             <p>click the button for answer</p>
             <textarea
+               className="border border-red-400 rounded-md p-2 mb-4"
               value={question}
               onChange={(e) => setQuestion(e.target.value)}
               cols="40"
@@ -50,6 +53,7 @@ export function ChatPage(){
               placeholder="Type your message here..."
             />
             <button onClick = {handleclick} className= "bg-blue-500 text-white px-4">button</button>
+            <pre>{answer}</pre>
         </div>
     );
 };
