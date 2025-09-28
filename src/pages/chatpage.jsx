b5eeba677c5423dacfa62e470d0916d2a2c3a1a2
sasankgit <sasank4pr@gmail.com>
2025-09-28 15:55:51 +0530

    fist commit

---
+++ src/pages/chatpage.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const ChatPage = () => {
+    return(
+        <div>
+            <h1>Chat Page</h1>
+            <p>welcome to the chat page</p>
+        </div>
+    );
+};
